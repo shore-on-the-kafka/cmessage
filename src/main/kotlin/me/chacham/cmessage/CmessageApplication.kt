556b16a8c95db93f70433592f6ea0beeffa994ab
chacham <ccm0702@naver.com>
2025-01-30 16:49:18 +0900

    Init SpringBoot project

---
+++ src/main/kotlin/me/chacham/cmessage/CmessageApplication.kt
@@ -0,0 +1,11 @@
+package me.chacham.cmessage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CmessageApplication
+
+fun main(args: Array<String>) {
+	runApplication<CmessageApplication>(*args)
+}
